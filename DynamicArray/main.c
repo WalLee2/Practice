d174cde61a881a518285ab9ddadfc8498fe510a4
Walton Lee <mr.waltonlee@me.com>
2017-08-13 16:59:18 -0700

    test file

---
+++ DynamicArray/main.c
@@ -0,0 +1,20 @@
+#include "vector.h"
+
+int main(void)
+{
+  Vector vector;
+  int i;
+
+  printf("Initiating variables\n");
+  vector_init(&vector);
+  printf("I've finished initiating!\n");
+  for (i = 200; i > -50; i--)
+    {
+      vector_append(&vector, i);
+    }
+  printf("Done with vector append!\n");
+  vector_set(&vector, 4552, 21312984);
+  printf("Value at 27: %d\n", vector_get(&vector, 27));
+  vector_free(&vector);
+  return (0);
+}
