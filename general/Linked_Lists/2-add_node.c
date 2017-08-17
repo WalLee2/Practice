d0a17e35f95e2c8e47345994df7056c967f196f1
Walton Lee <mr.waltonlee@me.com>
2017-08-17 12:29:31 -0700

    Adding comments

--- general/Linked_Lists/2-add_node.c
+++ general/Linked_Lists/2-add_node.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
- *
+ * add_node - Function that adds a node to the beginning of the list
+ * @head: A pointer that points to the beginning of the list
+ * @str: The string to be added to the linked list
+ * Return - The newly added node to the beginning of the list
  */
 list_t *add_node(list_t **head, const char *str)
 {
