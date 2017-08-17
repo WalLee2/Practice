01d383bae6d59c5e4694d9d3db90eee6651b809b
Walton Lee <mr.waltonlee@me.com>
2017-08-17 12:33:44 -0700

    adding comments

--- general/Linked_Lists/4-free_list.c
+++ general/Linked_Lists/4-free_list.c
@@ -1,5 +1,8 @@
 #include "lists.h"
-
+/**
+ * free_list - function that frees the entire list
+ * @head: The pointer to the beginning of the list
+ */
 void free_list(list_t *head)
 {
 	list_t *ptr;
