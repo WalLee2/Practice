4c5e18c19d260f9be76090e9037dd0b757543128
Walton Lee <mr.waltonlee@me.com>
2017-08-31 18:02:14 -0700

    clean up errors

--- getline/main.c
+++ getline/main.c
@@ -8,7 +8,7 @@
 int main(int ac, char *av[])
 {
 	FILE *fd;
-	char *buff;
+	char *buff, *buff2;
 	size_t size;
 	int status;
 
@@ -28,5 +28,13 @@ int main(int ac, char *av[])
 	status = grabline(&buff, &size, fd);
 	printf("errno: %d\n", errno);
 	printf("status: %d\n", status);
+	free(buff);
+	buff2 = NULL;
+	status = getline(&buff2, &size, fd);
+	printf("getline status: %d\n", status);
+	printf("%s\n", strerror(errno));
+	printf("getline string: %s\n", buff2);
+	free(buff2);
+	fclose(fd);
 	return (0);
 }
