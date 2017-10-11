a7af00a08f023436041e083f70b514fa015e8a0c
Walton Lee <mr.waltonlee@me.com>
2017-10-11 12:44:26 -0700

    Fixed core dumped

--- Coursera/Algorithm_Toolbox/C++/week4/naiveMultiplyPolynomials.cpp
+++ Coursera/Algorithm_Toolbox/C++/week4/naiveMultiplyPolynomials.cpp
@@ -5,12 +5,12 @@ using std::cin;
 using std::cout;
 using std::vector;
 
-vector<int> multiplyPoly(vector<int> A, vector<int> B, vector<int> &C, int n) {
+vector<int> multiplyPoly(vector<int> &A, vector<int> &B, vector<int> &C, int n) {
 	int i, j;
 
 	for (i = 0; i < (n - 1); ++i) {
 		for (j = 0; j < (n - 1); ++j) {
-			C[i + j] = C[i + j] + (A[i] * B[j]);
+			C[i + j] += (A[i] * B[j]);
 		}
 	}
 	return (C);
@@ -20,7 +20,7 @@ int main(void) {
 	int n, i, sum;
 
 	cin >> n;
-	sum = n + n;
+	sum = n + n - 1;
 	vector<int> A(n);
 	vector<int> B(n);
 	vector<int> C(sum);
@@ -31,13 +31,13 @@ int main(void) {
 	for (i = 0; i < n; ++i) {
 		cin >> B[i];
 	}
-	for (i = 0; i < (n * n); ++i) {
+	for (i = 0; i < sum; ++i) {
 		C[i] = 0;
 	}
-	C = multiplyPoly(A, B, C, n);
-	for (i = 0; i < (n * n); ++i) {
+	C = multiplyPoly(A, B, C, sum);
+	for (i = 0; i < sum; ++i) {
 		cout << C[i];
-		if (i < (n + n) - 1) {
+		if (i < sum - 1) {
 			cout << ", ";
 		}
 	}
