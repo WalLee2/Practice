af371c600203f1ec5628366c27cbafa836fdb53c
Walton Lee <mr.waltonlee@me.com>
2017-10-11 11:41:43 -0700

    Multiplying polynomials

---
+++ Coursera/Algorithm_Toolbox/C++/week4/naiveMultiplyPolynomials.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::vector;
+
+vector<int> multiplyPoly(vector<int> A, vector<int> B, vector<int> &C, int n) {
+	int i, j;
+
+	for (i = 0; i < (n - 1); ++i) {
+		for (j = 0; j < (n - 1); ++j) {
+			C[i + j] = C[i + j] + (A[i] * B[j]);
+		}
+	}
+	return (C);
+}
+
+int main(void) {
+	int n, i, sum;
+
+	cin >> n;
+	sum = n + n;
+	vector<int> A(n);
+	vector<int> B(n);
+	vector<int> C(sum);
+
+	for (i = 0; i < n; ++i) {
+		cin >> A[i];
+	}
+	for (i = 0; i < n; ++i) {
+		cin >> B[i];
+	}
+	for (i = 0; i < (n * n); ++i) {
+		C[i] = 0;
+	}
+	C = multiplyPoly(A, B, C, n);
+	for (i = 0; i < (n * n); ++i) {
+		cout << C[i];
+		if (i < (n + n) - 1) {
+			cout << ", ";
+		}
+	}
+	cout << '\n';
+	return (0);
+}
