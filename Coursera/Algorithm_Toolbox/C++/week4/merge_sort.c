052f58034d8be9535eb0934eff47e37c8a96c039
Walton Lee <mr.waltonlee@me.com>
2017-10-17 08:54:22 -0700

    removed debugging lines

--- Coursera/Algorithm_Toolbox/C++/week4/merge_sort.c
+++ Coursera/Algorithm_Toolbox/C++/week4/merge_sort.c
@@ -15,52 +15,36 @@ void merge(int arr[], size_t low, size_t mid, size_t hi) {
 	}
 
 	i = j = 0;
-	printf("arr[z]: ");
 	for (z = low; i < lh && j < uh; z++) {
 		if (lower_half[i] <= upper_half[j]) {
 			arr[z] = lower_half[i];
-			printf("%d ", arr[z]);
 			i++;
 		} else {
 			arr[z] = upper_half[j];
-			printf("%d ", arr[z]);
 			j++;
 		}
 	}
-	printf("\n");
 	while (i < lh) {
 		arr[z] = lower_half[i];
-		printf("appending lower_half: %d ", lower_half[i]);
 		i++;
 		z++;
 	}
 	while (j < uh) {
 		arr[z] = upper_half[j];
-		printf("appending upper_half: %d ", upper_half[j]);
 		j++;
 		z++;
 	}
-	printf("\n");
 }
 
 void merge_sort(int arr[], size_t low, size_t high) {
 	size_t middle;
 
 
-	printf("Outside low: %lu\n", low);
-	printf("Outside high: %lu\n\n", high);
 	if (low < high) {
 		middle = low + (high - low) / 2;
-		printf("First recursion low: %lu\n", low);
-		printf("First recursion high: %lu\n\n", high);
 		merge_sort(arr, low, middle);
-		printf("Second recursion low: %lu\n", low);
-		printf("Second recursion high: %lu\n\n", high);
 		merge_sort(arr, (middle + 1), high);
-		printf("I'm going to merge!\n\n");
 		merge(arr, low, middle, high);
-		printf("Low after merge: %lu\n", low);
-		printf("High after merge: %lu\n\n", high);
 	}
 }
 void printArray(int A[], size_t size)
