79b229de41262f04b635d5e32e7c6f84e4838266
Walton Lee <mr.waltonlee@me.com>
2017-11-16 19:42:35 -0800

    fixed double free

--- leetcode/intersection_two_arrays.cpp
+++ leetcode/intersection_two_arrays.cpp
@@ -5,41 +5,38 @@
 using std::cout;
 using std::vector;
 
-int b_search(vector<int> &nums, int target, vector<int> &found_idx) {
-	int low, high, mid, i;
+int b_search(vector<int> &nums, int target) {
+	int low, high, mid;
 
-	low = i = 0;
+	low = 0;
 	high = (nums.size() - 1);
 	while (low <= high) {
 		mid = low + (high - low) / 2;
-		if (nums[mid] == target && found_idx[i] != mid) {
-			found_idx.push_back(mid);
+		if (nums[mid] == target) {
 			return (mid);
 		} else if (nums[mid] < target) {
 			low = mid + 1;
 		} else {
 			high = mid - 1;
 		}
-		i++;
 	}
 	return (-1);
 }
 
 vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
 	vector<int> returned_arr;
-	vector<int> found_idx;
 	int index;
 	size_t i;
 
 	if (nums1.empty() || nums2.empty()) {
 		return (returned_arr);
 	}
-	sort(nums1.begin(), nums2.end());
+	sort(nums1.begin(), nums1.end());
 	sort(nums2.begin(), nums2.end());
 	i = 0;
 	while (i < nums1.size() && i < nums2.size()) {
 		if (nums1.size() > nums2.size()) {
-			index = b_search(nums1, nums2[i], found_idx);
+			index = b_search(nums1, nums2[i]);
 			if (index != -1) {
 				returned_arr.push_back(nums1[index]);
 				i++;
@@ -47,7 +44,7 @@ vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
 				i++;
 			}
 		} else {
-			index = b_search(nums2, nums1[i], found_idx);
+			index = b_search(nums2, nums1[i]);
 			if (index != -1) {
 				returned_arr.push_back(nums1[index]);
 				i++;
@@ -60,14 +57,18 @@ vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
 }
 int main(void) {
         vector<int> nums1, nums2;
-        nums1.insert(nums1.end(), {1});
-        nums2.insert(nums2.end(), {1, 1});
-        vector<int> intersection(1);
+        nums1.insert(nums1.end(), {1, 2, 2, 1});
+        nums2.insert(nums2.end(), {1, 2, 2});
+        vector<int> intersection(2);
         size_t i;
 
         intersection = intersect(nums1, nums2);
         for (i = 0; i < intersection.size(); i++) {
-                cout << "Number in array" << intersection[i] << '\n';
+                cout << intersection[i];
+		if (i < intersection.size() - 1) {
+			cout << ", ";
+		}
         }
+	cout << '\n';
         return (0);
 }
